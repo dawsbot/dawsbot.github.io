10b5b6ea580cefadc344b314201e9583fd40a7f3
Dawson Botsford <3408480+dawsbot@users.noreply.github.com>
2015-05-20 23:38:23 -0600

    Closes #4. Also added Mountain View

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,7 +52,4 @@ jQuery(document).ready(function($) {
         }
     );
     
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "dawsonbotsford", selector: "#ghfeed" });
-
 });
